e15dfada7f8e12e6a2cba23a583ae399c29229e7
pooas <cyroustrader@gmail.com>
2023-08-19 14:46:56 +0330

    Vector iter..

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include<iostream>
-
+#include<vector>
+#include<array>
 struct coord
+
 {
     char a;
     char b;
 };
 
 
+std::vector<int> v{936,3,86,9,12};
+
+
 int main(){
     
+	
     coord test{120, 63};
-
+	coord test3{12,9};
+	coord *test4 = &test3;
     coord *test2 = &test;
+
+	
+
+	auto b1 = begin(v);
+	auto e1 = end(v);
+
 
     char v = test2->a;
     char w = test2->b;
@@ -22,7 +35,13 @@ int main(){
     char s = (*test2).a;
     char t = (*test2).b;
 	
+
 	std::cout << "Pointer is here...\n";
+	
 	std::cout << s << t << "  ::  " << v << w << "\n";
-    return 0;
+
+	std::cout << "The pinter address is ... " <<  test2 << "\n";
+	std::cout << "The second pointer address is ... " << test4 << "\n";
+    std::cout << *b1 << "  :  "  << *e1 <<"\n";
+	return 0;
 }
