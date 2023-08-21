ab0b79b6b0935d388fbde05ede23ffe09164d2c9
pooas <cyroustrader@gmail.com>
2023-08-21 16:47:57 +0330

    Add foo function to show stack and heap

--- stack.cpp
+++ stack.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 
+void foo(std::vector<int> w, int x){
+	w[0] = x;
+}
+
 
 int main(){
+
+	std::vector<int> v;
+	v.push_back(3);
+	v.push_back(6);
+
+	foo(v, 8);
+
+	std::cout << *v << " \n";
+
 	int i = 4;
 	for(int j =0; j < i; j++){
 		if (j > 2){
