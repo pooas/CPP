372a221339954d2d1395ddb3cdf97fe3715b13a3
pooas <cyroustrader@gmail.com>
2023-08-23 11:32:10 +0330

    stack

--- stack.cpp
+++ stack.cpp
@@ -1,19 +1,22 @@
 #include<iostream>
 
+int main(){
+
 void foo(std::vector<int> w, int x){
 	w[0] = x;
 }
 
 
-int main(){
+
 
 	std::vector<int> v;
 	v.push_back(3);
 	v.push_back(6);
 
 	foo(v, 8);
+	std::cout << v[0] << "\n";
 
-	std::cout << *v << " \n";
+
 
 	int i = 4;
 	for(int j =0; j < i; j++){
